e174ede07a9e885d37a465fa8d436deb52093fd8
Moyad_faraj <moyadfarajahmad@gmail.com>
2024-03-09 02:43:50 +0300

    Second commmit

--- src/main/java/com/atmbank/ATMbackend/service/AtmServiceImpl.java
+++ src/main/java/com/atmbank/ATMbackend/service/AtmServiceImpl.java
@@ -19,6 +19,9 @@ public class AtmServiceImpl implements AtmService {
 
     private UserAccountRepository userAccountRepository;
     private ATMRepository atmRepository;
+    /*
+    moayad faraj ahmad
+     */
 
     @Autowired
     public AtmServiceImpl(UserAccountRepository userAccountRepository, ATMRepository atmRepository) {
